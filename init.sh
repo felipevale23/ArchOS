4cf0625a2294cb802afa7f2631dec8866d2ef412
Felipe Vale <74729750+felipevale23@users.noreply.github.com>
2022-07-16 18:09:14 -0300

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------
+#
+#           .o.                          oooo               .oooooo.    .oooooo..o 
+#          .888.                         `888              d8P'  `Y8b  d8P'    `Y8 
+#         .8"888.     oooo d8b  .ooooo.   888 .oo.        888      888 Y88bo.      
+#        .8' `888.    `888""8P d88' `"Y8  888P"Y88b       888      888  `"Y8888o.  
+#       .88ooo8888.    888     888        888   888       888      888      `"Y88b 
+#      .8'     `888.   888     888   .o8  888   888       `88b    d88' oo     .d8P 
+#     o88o     o8888o d888b    `Y8bod8P' o888o o888o       `Y8bood8P'  8""88888P'   
+#                   
+#                    Arch Linux Post Install Setup and Config
+#-------------------------------------------------------------------------
+
+echo
+echo "SHALL WE BEGIN?!"
+echo
+
+# Initialize keyring
+echo "pacman-key --init"
+echo "pacman-key --populate"
+echo "pacman -Syy archlinux-keyring"
+
+PKGS=(
+            'git'            
+            'base-devel'      
+)
+
+for PKG in "${PKGS[@]}"; do
+    echo "INSTALLING ${PKG}"
+    sudo pacman -S "$PKG" --noconfirm --needed
+done
+
+echo
+echo "Done!"
+echo
