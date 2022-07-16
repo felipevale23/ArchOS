e604e24ebccf1a4826f5340fd340b858852df889
Felipe Vale <74729750+felipevale23@users.noreply.github.com>
2022-07-16 17:55:43 -0300

    Create theme.sh

---
+++ theme.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------
+#
+#           .o.                          oooo               .oooooo.    .oooooo..o 
+#          .888.                         `888              d8P'  `Y8b  d8P'    `Y8 
+#         .8"888.     oooo d8b  .ooooo.   888 .oo.        888      888 Y88bo.      
+#        .8' `888.    `888""8P d88' `"Y8  888P"Y88b       888      888  `"Y8888o.  
+#       .88ooo8888.    888     888        888   888       888      888      `"Y88b 
+#      .8'     `888.   888     888   .o8  888   888       `88b    d88' oo     .d8P 
+#     o88o     o8888o d888b    `Y8bod8P' o888o o888o       `Y8bood8P'  8""88888P'   
+#                   
+#                    Arch Linux Post Install Setup and Config
+#-------------------------------------------------------------------------
+
+
+echo
+echo "INSTALLING MONTEREY KDE THEME"
+echo
+
+cd "/tmp"
+echo "CLONING: YAY"
+
+echo
+echo "git clone https://github.com/vinceliuice/Monterey-kde.git"
+echo
+
+chmod +x install.sh
+
+echo "./install.sh"
